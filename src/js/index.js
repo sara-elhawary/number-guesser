12bcce2052bc94ad25033d2de55d48eb14b4ce21
sarah elhwarii <sarah.elhwarii92@gmail.com>
2020-04-25 03:39:31 +0300

    UI elements selection

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-console.log("everything is working fine");
+const game = document.querySelector("#game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
+  message = document.querySelector(".message");
+
+minNum.textContent = min;
+maxNum.textContent = max;
