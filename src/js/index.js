5debd0bf189a17310d55c410d53e92565823145f
sarah elhwarii <sarah.elhwarii92@gmail.com>
2020-04-25 03:41:18 +0300

    button functionality

--- src/js/index.js
+++ src/js/index.js
@@ -12,3 +12,26 @@ const game = document.querySelector("#game"),
 
 minNum.textContent = min;
 maxNum.textContent = max;
+
+guessBtn.addEventListener("click", guessBtnFunction);
+function guessBtnFunction() {
+  let guess = parseInt(guessInput.value);
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+    setTimeout(clearMsg, 3000);
+  }
+  if (guess === winningNumber) {
+    gameOver(true, `${guess} is correct,You WIN !`);
+  } else {
+    guessesleft -= 1;
+    if (guessesleft === 0) {
+      gameOver(false, `Game Over sucker!,you are out of guesses`);
+    } else {
+      setMessage(
+        `${guess} is incorrect!,you have ${guessesleft} guesses left.`,
+        "red"
+      );
+      guessInput.value = "";
+    }
+  }
+}
